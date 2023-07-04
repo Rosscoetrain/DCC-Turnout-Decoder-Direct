56906c1fc31884cd2249e428ebfa126c847901a8
Rosscoetrain <iam@rosscoe.com>
2023-07-04 19:08:04 +1000

    Update ino file

--- DCC-Turnout-Decoder-Direct.ino
+++ DCC-Turnout-Decoder-Direct.ino
@@ -1,6 +1,6 @@
 #include <NmraDcc.h>
 #include "PinPulser.h"
-// This Example shows how to use the library as a DCC Accessory Decoder to drive 8 Pulsed Turnouts
+// This is a DCC Accessory Decoder to drive 8 Pulsed Turnouts
 
 // You can print every DCC packet by un-commenting the line below
 //#define NOTIFY_DCC_MSG
@@ -13,7 +13,7 @@
 
 // Un-Comment the line below to force CVs to be written to the Factory Default values
 // defined in the FactoryDefaultCVs below on Start-Up
-//#define FORCE_RESET_FACTORY_DEFAULT_CV
+#define FORCE_RESET_FACTORY_DEFAULT_CV
 
 // Un-Comment the line below to Enable DCC ACK for Service Mode Programming Read CV Capablilty 
 //#define ENABLE_DCC_ACK  15  // This is A1 on the Iowa Scaled Engineering ARD-DCCSHIELD DCC Shield
